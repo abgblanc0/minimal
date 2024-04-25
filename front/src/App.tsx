65714fab096481c0f0faa895f45feeaf80fd5047
Alejandro Blanco <abg_blanco@hotmail.com>
2024-04-25 12:28:44 +0200

    feat: project started

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="m-10">
+      HOLA
+    </div>
+  )
+}
+
+export default App
---
+++ front/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
