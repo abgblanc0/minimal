d2e3cbe1acee0704c3c37c031b94de8453f5f062
Alejandro Blanco <abg_blanco@hotmail.com>
2024-04-26 13:20:43 +0200

    feat: new routes

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,12 +1,33 @@
+#![allow(unused)]
 #[macro_use]
 extern crate rocket;
 
+use db::establish_connection;
+use models::user::User;
+use rocket::serde::json::Json;
+use controllers::user::*;
+
+mod controllers;
 mod db;
 mod models;
 mod schema;
+mod script;
 
-use crate::models::user::User;
 
+#[get("/users")]
+fn users() -> Json<Vec<User>> {
+    let mut conn = establish_connection();
+    Json(get_users(&mut conn))
+}
+
+
+//TODO: return 404 if not found...
+#[get("/users/<user_id>")] 
+fn user_by_id(user_id: i32) -> Json<User> {
+    let mut conn = establish_connection();
+    Json(get_user(&mut conn, user_id).unwrap())
+}
+ 
 #[get("/")]
 fn index() -> &'static str {
     "Hello"
@@ -14,5 +35,6 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    script::insert_data();
+    rocket::build().mount("/", routes![index, users, user_by_id])
 }
