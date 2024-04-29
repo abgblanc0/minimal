32dbb67db6711170b1c1c4f87a18f467a85d1af1
Alejandro Blanco <abg_blanco@hotmail.com>
2024-04-29 13:57:36 +0200

    feat: new routes, pool atached

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,40 +1,17 @@
 #![allow(unused)]
+
 #[macro_use]
 extern crate rocket;
-
-use db::establish_connection;
-use models::user::User;
-use rocket::serde::json::Json;
-use controllers::user::*;
 
 mod controllers;
 mod db;
 mod models;
 mod schema;
-mod script;
+mod routes;
+mod errors;
 
-
-#[get("/users")]
-fn users() -> Json<Vec<User>> {
-    let mut conn = establish_connection();
-    Json(get_users(&mut conn))
-}
-
-
-//TODO: return 404 if not found...
-#[get("/users/<user_id>")] 
-fn user_by_id(user_id: i32) -> Json<User> {
-    let mut conn = establish_connection();
-    Json(get_user(&mut conn, user_id).unwrap())
-}
- 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello"
-}
 
 #[launch]
 fn rocket() -> _ {
-    script::insert_data();
-    rocket::build().mount("/", routes![index, users, user_by_id])
+    rocket::build().attach(db::init_db()).mount("/", routes![routes::user::users, routes::user::user_by_id, routes::user::post_user, routes::user::patch_user])
 }
