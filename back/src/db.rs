e71c538ae52e00e3409f6fa1841d68fd9354e740
Alejandro Blanco <abg_blanco@hotmail.com>
2024-04-30 13:27:12 +0200

    fix: not needed

--- back/src/db.rs
+++ back/src/db.rs
@@ -4,7 +4,6 @@ use rocket::fairing::AdHoc;
 use std::env;
 use diesel::r2d2::{ConnectionManager, Pool};
 
-type PgPool = Pool<ConnectionManager<PgConnection>>;
 
 pub fn init_db() -> AdHoc {
     dotenv().ok();
@@ -18,34 +17,3 @@ pub fn init_db() -> AdHoc {
         rocket.manage(pool.clone()) 
     }) 
 }
-/* 
-use rocket::fairing::AdHoc;
-use std::env;
-
-pub mod customer;
-
-pub fn init() -> AdHoc {
-    AdHoc::on_ignite("Connecting to MongoDB", |rocket| async {
-        match connect().await {
-            Ok(database) => rocket.manage(database),
-            Err(error) => {
-                panic!("Cannot connect to instance:: {:?}", error)
-            }
-        }
-    })
-}
-
-async fn connect() -> mongodb::error::Result<Database> {
-    let mongo_uri = env::var("MONGO_URI").expect("MONGO_URI is not found.");
-    let mongo_db_name = env::var("MONGO_DB_NAME").expect("MONGO_DB_NAME is not found.");
-
-    let client_options = ClientOptions::parse(mongo_uri).await?;
-    let client = Client::with_options(client_options)?;
-    let database = client.database(mongo_db_name.as_str());
-
-    println!("MongoDB Connected!");
-
-    Ok(database)
-}
-
-*/
