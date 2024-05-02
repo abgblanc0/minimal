e1c0c7b793154a688f4a2305c700f885963fd594
Alejandro Blanco <abg_blanco@hotmail.com>
2024-05-02 13:54:57 +0200

    feat: cors enable same origin

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,9 +1,12 @@
 #![allow(unused)]
 
+use cors::CORS;
+
 #[macro_use]
 extern crate rocket;
 
 mod controllers;
+mod cors;
 mod db;
 mod errors;
 mod models;
@@ -12,29 +15,28 @@ mod schema;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(db::init_db())
-    .mount(
-        "/users",
-        routes![
-            routes::user::users,
-            routes::user::user_by_id,
-            routes::user::post_user,
-            routes::user::patch_user,
-            routes::user::delete_user
-        ],
-    ).mount(
-        "/posts", 
-        routes![
-            routes::post::posts,
-            routes::post::posts_by_user_id,
-            routes::post::post_post,
-            routes::post::patch_post,
-            routes::post::delete_post,
-        ],
-    ).mount(
-        "/topics",
-        routes![
-            routes::topic::topics
-        ]
-    )
+    rocket::build()
+        .attach(db::init_db())
+        .attach(CORS)
+        .mount(
+            "/users",
+            routes![
+                routes::user::users,
+                routes::user::user_by_id,
+                routes::user::post_user,
+                routes::user::patch_user,
+                routes::user::delete_user
+            ],
+        )
+        .mount(
+            "/posts",
+            routes![
+                routes::post::posts,
+                routes::post::posts_by_user_id,
+                routes::post::post_post,
+                routes::post::patch_post,
+                routes::post::delete_post,
+            ],
+        )
+        .mount("/topics", routes![routes::topic::topics])
 }
